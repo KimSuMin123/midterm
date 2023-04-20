c42be260879deca12c04b398a26c960c3b7801a1
KimSuMin123 <tntn211@naver.com>
2023-04-20 13:35:16 +0900

    두번째 커밋

--- midterm/src/App.js
+++ midterm/src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [fruit, setfruit] = useState('');
+
+  const apple = e => {
+    setfruit("사과.");
+  }
+  const banana = e => {
+    setfruit("바나나.");
+  }
+  const orange = e => {
+    setfruit("오랜지.");
+  }
+  
   return (
     <div className="App">
-     
+      <h1>어떤 과일을 좋아하나요? {fruit} </h1>
+      <button onClick = {apple}>사과</button>
+      <button onClick = {orange}>오랜지</button>
+      <button onClick = {banana}>바나나</button>
     </div>
   );
 }
+
 
 export default App;
